6856be6dd7125e72daeadb2c5547ece5ab18ebb2
unknown <juan.carruthers@hotmail.com>
2020-05-27 21:28:47 -0300

    Modificaciones en el metodo corrida de parcial2. Funciona la definicion dinamica de los servidores

--- parcial2.m
+++ parcial2.m
@@ -28,14 +28,14 @@ classdef parcial2
     methods (Static)
         
 
-        function corrida (p_mediaClientes, p_cantHoras, p_tServ, p_maxTiempoEspera, p_maxTiempoOcio, p_cantServidores)
+        function corrida (p_mediaClientes, p_cantHoras, p_tServ, p_cantServidores)
             import pkg.guia5.*;          
             % Arreglo con los tiempos entre las llegadas de cada sujeto/cliente
             cantClientes = guia5.poisson(p_mediaClientes, 1);          
             %llegadaACola = zeros(1, cantClientes);
             tiemposEnCola = zeros(1, cantClientes);
             tiempo = 0;
-            tiemposOcioServ = 0;
+            tiemposOcioServTotal = 0;
             tablaResultados = zeros(cantClientes,9);
             
             
@@ -45,11 +45,13 @@ classdef parcial2
             tiemposServicio = guia5.exponencial(p_tServ, cantClientes);
 
 
-
             llegadaACola = 0:(p_cantHoras*60)/cantClientes:(p_cantHoras + 1)*60;
             tablaResultados(:,2) = llegadaACola(1,1:1:cantClientes).';
+            
+            % Cantidad de tiempo que transcurre entre las llegadas de 
+            % clientes/sujetos consecutivos
+            tablaResultados(:, 4) = (p_cantHoras*60)/cantClientes;
             tablaResultados(1, 4) = 0;
-            memoriaMin = zeros(1,p_cantServidores);
             
             
             for i = 1 : cantClientes 
@@ -65,6 +67,8 @@ classdef parcial2
                     if servLibre
                          if servidores(1,j) <= 0
                             servidores(2,j) = servidores(2,j) + llegadaACola(1, i);
+                            tablaResultados(i, 9 + j - 1) = servidores(2,j);
+                            tiemposOcioServTotal = tiemposOcioServTotal + servidores(2,j);
                          end                           
                     else                                                  
                         if servidores(1,j) <= 0
@@ -74,10 +78,13 @@ classdef parcial2
                     end
                 end
                 
+                
                 llegadaACola(1, :) = llegadaACola(1, :) - llegadaACola(1, i);
                 sujetosCola = 0;
                 
-                
+                %
+                % Asignacion del servidor que se libere antes
+                %
                 if not(servLibre) 
                     % Cuenta las personas en la cola hasta que el sujeto/cliente
                     % termina de ser atendido
@@ -90,42 +97,16 @@ classdef parcial2
                         end
                     end
                     
-                    [minimo, indiceMin] =  min(servidores(1,p_cantServidores));
-                    %%
-                    %Donde se presentan los problemas, cuando se empiezan a
-                    %encolar los clientes en la fila
-                    %
-                    if sujetosCola > 0
-                        if sujetosCola == 1
-                            memoriaMin = servidores(1,:);
-                        else
-                            [minimo, indiceMin] =  min(memoriaMin);
-                        end                            
-                        memoriaMin(1,indiceMin) = memoriaMin(1,indiceMin) + tiemposServicio(1,i - 1);
-                        [minimo, indiceMin] =  min(memoriaMin);
-                        fprintf('Index=%i. Value=%.4f\n',minimo,minimo);
-                    end
-                    %
-                    %
-                    %
-                    %%
+                    [minimo, indiceMin] =  min(servidores(1,:));                
                     
                     tiemposEnCola(1, i) = minimo;
                     tiempo = tiempo + minimo;
                     servidores(1,:) = servidores(1,:) - minimo;
                     llegadaACola(1, :) = llegadaACola(1, :) - minimo;
-                    servidores(1,indiceMin) = tiemposServicio(1,i);
-                    
+                    servidores(1,indiceMin) = tiemposServicio(1,i);                    
                     
                 end
-                              
-                
-               
-                if i > 1                    
-                    % Cantidad de tiempo que transcurre entre las llegadas de
-                    % clientes/sujetos consecutivos
-                    tablaResultados(i, 4) = tablaResultados(i,2) - tablaResultados(i-1,2);
-                end
+                                           
                 
                 % Numero de sujeto/cliente que llega a la cola
                 tablaResultados(i, 1) = i;
@@ -143,10 +124,11 @@ classdef parcial2
                 % ser atendidos
                 tablaResultados(i, 8) = sujetosCola;
                 % Cantidad de tiempo que el servidor no esta atendiendo
-                %tablaResultados(i, 9) = tiemposOcioServ;
+                tablaResultados(i, 9 + p_cantServidores) = tiemposOcioServTotal;
                
             end   
-            queueing.mostrarResultadoCorrida(tablaResultados);
+            parcial2.mostrarResultadoCorrida(tablaResultados);
+            %disp(tablaResultados);
         end
         
 
@@ -154,6 +136,10 @@ classdef parcial2
             
             fprintf('\n\n\t\t\tEjecucion de Modelo de Colas\n\n');
             colNames = {'Sujeto', 'TiempoLlegadaACola','TiempoServicio','TiempoEntreLlegadas','TiempoEnCola','TiempoEnSistema','TiempoSalida', 'PersonasEnCola'};
+            for i = 9 : size(p_tabla,2) - 1
+                colNames{1,i} = strcat('TiempoOcioServidor', num2str(i - 8,'%i'));               
+            end
+            colNames{1,size(p_tabla,2)} = 'TiempoOcioServidorTotal';
             sTable = array2table(p_tabla,'VariableNames',colNames);
             disp (sTable);
             
